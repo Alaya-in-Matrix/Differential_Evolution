a50197d57482cd99cab3162e21c0572e1b060a76
alaya <lvwenlong_lambda@qq.com>
2016-02-03 22:25:33 +0800

    new DE Finished, but not tested, may fail to compile

--- inc/DifferentialEvolution.cpp
+++ inc/DifferentialEvolution.cpp
@@ -1,42 +1,22 @@
 #include "DifferentialEvolution.h"
 #include "util.h"
+#include <iostream>
 #include <random>
 #include <omp.h>
 #include <cassert>
 #include <numeric>
+#include <string>
 using namespace std;
 mt19937_64 engine(random_device{}());
-Solution operator-(const Solution s1, const Solution s2)
+double IMutator::boundary_constraint(pair<double, double> rg, double val) const noexcept
 {
-    assert(s1.size() == s2.size());
-    vector<double> result = s1;
-    for (size_t i = 0; i < s1.size(); ++i)
-    {
-        result[i] -= s2[i];
-    }
-    return result;
+    assert(rg.first <= rg.second);
+    uniform_real_distribution<double> distr(rg.first, rg.second);
+    return rg.first <= val && val <= rg.second ? val : distr(engine);
 }
-Solution operator+(const Solution s1, const Solution s2)
+vector<Solution> Mutator_Rand_1::mutation(const DE& de)
 {
-    assert(s1.size() == s2.size());
-    vector<double> result = s1;
-    for (size_t i = 0; i < s1.size(); ++i)
-    {
-        result[i] += s2[i];
-    }
-    return result;
-}
-Solution operator*(double f, const Solution s)
-{
-    vector<double> result = s;
-    for (size_t i = 0; i < s.size(); ++i)
-    {
-        result[i] *= f;
-    }
-    return result;
-}
-vector<Solution> Mutator_Rand_1::mutation(const DE& de, const vector<Solution>& population)
-{
+    const vector<Solution>& population = de.population();
     assert(population.size() == de.np());
     uniform_int_distribution<size_t> i_distr(0, population.size() - 1);
     vector<Solution> mutated = population;
@@ -45,51 +25,70 @@ vector<Solution> Mutator_Rand_1::mutation(const DE& de, const vector<Solution>& 
         size_t r1 = random_exclusive<size_t>(i_distr);
         size_t r2 = random_exclusive<size_t>(i_distr, vector<size_t> {r1});
         size_t r3 = random_exclusive<size_t>(i_distr, vector<size_t> {r1, r2});
-        m = population[r1] + de.f() * (population[r2] - population[r3]);
+        for (size_t i = 0; i < de.dimension(); ++i)
+        {
+            m[i] = population[r1][i] + de.f() * (population[r2][i] - population[r3][i]);
+            m[i] = boundary_constraint(de.range(i), m[i]);
+        }
     }
     return mutated;
 }
-vector<Solution> Mutator_Best_1::mutation(const DE& de, const vector<Solution>& population)
+vector<Solution> Mutator_Best_1::mutation(const DE& de)
 {
+    const vector<Solution>& population = de.population();
     assert(population.size() == de.np());
     uniform_int_distribution<size_t> i_distr(0, population.size() - 1);
     vector<Solution> mutated = population;
-    size_t best_idx = de.find_best(population);
+    size_t best_idx = de.find_best();
     for (Solution& m : mutated)
     {
         size_t r1 = random_exclusive<size_t>(i_distr, vector<size_t> {best_idx});
         size_t r2 = random_exclusive<size_t>(i_distr, vector<size_t> {best_idx, r1});
-        m = population[best_idx] + de.f() * (population[r1] - population[r2]);
+        for (size_t i = 0; i < de.dimension(); ++i)
+        {
+            m[i] = population[best_idx][i] + de.f() * (population[r1][i] - population[r2][i]);
+            m[i] = boundary_constraint(de.range(i), m[i]);
+        }
     }
     return mutated;
 }
-vector<Solution> Mutator_Best_2::mutation(const DE& de, const vector<Solution>& population)
+vector<Solution> Mutator_Best_2::mutation(const DE& de)
 {
+    const vector<Solution>& population = de.population();
     assert(population.size() == de.np());
     uniform_int_distribution<size_t> i_distr(0, population.size() - 1);
     vector<Solution> mutated = population;
-    size_t best_idx = de.find_best(population);
+    size_t best_idx = de.find_best();
     for (Solution& m : mutated)
     {
         size_t r1 = random_exclusive<size_t>(i_distr, vector<size_t> {best_idx});
         size_t r2 = random_exclusive<size_t>(i_distr, vector<size_t> {best_idx, r1});
         size_t r3 = random_exclusive<size_t>(i_distr, vector<size_t> {best_idx, r1, r2});
         size_t r4 = random_exclusive<size_t>(i_distr, vector<size_t> {best_idx, r1, r2, r3});
-        m = population[best_idx] + de.f() * (population[r1] - population[r2]) + de.f() * (population[r3] - population[r4]);
+        for (size_t i = 0; i < de.dimension(); ++i)
+        {
+            m[i] = population[best_idx][i] + de.f() * (population[r1][i] - population[r2][i]) + de.f() * (population[r3][i] - population[r4][i]);
+            m[i] = boundary_constraint(de.range(i), m[i]);
+        }
     }
     return mutated;
 }
-vector<Solution> Mutator_RandToBest_1::mutation(const DE& de, const vector<Solution>& population)
+vector<Solution> Mutator_RandToBest_1::mutation(const DE& de)
 {
+    const vector<Solution>& population = de.population();
     assert(population.size() == de.np());
     uniform_int_distribution<size_t> i_distr(0, population.size() - 1);
     vector<Solution> mutated = population;
-    size_t best_idx = de.find_best(population);
+    size_t best_idx = de.find_best();
     for (Solution& m : mutated)
     {
         size_t r1 = random_exclusive<size_t>(i_distr, vector<size_t> {best_idx});
         size_t r2 = random_exclusive<size_t>(i_distr, vector<size_t> {best_idx, r1});
-        m = m + de.f() * (population[best_idx] - m) + de.f() * (population[r1] - population[r2]);
+        for (size_t i = 0; i < de.dimension(); ++i)
+        {
+            m[i] = m[i] + de.f() * (population[best_idx][i] - m[i]) + de.f() * (population[r1][i] - population[r2][i]);
+            m[i] = boundary_constraint(de.range(i), m[i]);
+        }
     }
     return mutated;
 }
@@ -134,10 +133,10 @@ vector<Solution> Crossover_Exp::crossover(const DE& de, const vector<Solution>& 
     return trials;
 }
 pair<vector<Evaluated>, vector<Solution>> ISelector::select(const DE& de
-        , const vector<Solution>& targets
-        , const vector<Solution>& trials
-        , const vector<Evaluated>& target_results
-        , const vector<Evaluated>& trial_results)
+                                       , const vector<Solution>& targets
+                                       , const vector<Solution>& trials
+                                       , const vector<Evaluated>& target_results
+                                       , const vector<Evaluated>& trial_results)
 {
     assert(targets.size() == de.np() && de.np() == trials.size());
     assert(target_results.size() == de.np() && de.np() == trial_results.size());
@@ -154,14 +153,24 @@ pair<vector<Evaluated>, vector<Solution>> ISelector::select(const DE& de
     return make_pair(child_results, offspring);
 }
 pair<vector<Evaluated>, vector<Solution>> Selector_Epsilon::select(const DE& de
-        , const vector<Solution>& targets
-        , const vector<Solution>& trials
-        , const vector<Evaluated>& target_results
-        , const vector<Evaluated>& trial_results)
+                                       , const vector<Solution>& targets
+                                       , const vector<Solution>& trials
+                                       , const vector<Evaluated>& target_results
+                                       , const vector<Evaluated>& trial_results)
 {
-    auto ret        = ISelector::select(de, targets, trials, target_results, trial_results);
-    size_t curr_gen = de.curr_gen();
-    epsilon_level   = curr_gen > tc ? 0 : epsilon_0 * pow(1 - curr_gen / tc, cp);
+    if (de.curr_gen() == 1)
+    {
+        vector<double> violations(de.np(), numeric_limits<double>::infinity());
+        for (size_t i = 0; i < target_results.size(); ++i)
+            violations[i] = accumulate(target_results[i].second.begin(), target_results[i].second.end(), 0);
+        size_t cutoff = (size_t)(de.np() * theta);
+        partial_sort(violations.begin(), violations.begin() + cutoff, violations.end());
+        epsilon_0     = violations[cutoff - 1];
+        epsilon_level = epsilon_0;
+    }
+    auto ret      = ISelector::select(de, targets, trials, target_results, trial_results);
+    size_t gen    = de.curr_gen();
+    epsilon_level = gen > tc ? 0 : epsilon_0 * pow(1 - gen / tc, cp);
     return ret;
 }
 bool Selector_StaticPenalty::better(const Evaluated& r1, const Evaluated& r2)
@@ -195,4 +204,161 @@ bool Selector_Epsilon::better(const Evaluated& r1, const Evaluated& r2)
         return violation1 == violation2 ? fom1 <= fom2 : violation1 <= violation2;
     else
         return violation1 <= epsilon_level;
+}
+DE::DE(Objective func , const Ranges& rg
+       , MutationStrategy ms , CrossoverStrategy cs , SelectionStrategy ss
+       , unordered_map<string, double> extra
+       , double f , double cr , size_t np , size_t max_iter)
+    : _func(func), _f(f), _cr(cr), _curr_gen(0), _np(np), _dim(rg.size()), _max_iter(max_iter), _extra_conf(extra), _use_built_in_strategy(false)
+{
+    set_mutator(ms, extra);
+    set_crossover(cs, extra);
+    set_selector(ss, extra);
+}
+DE::DE(Objective func, const Ranges& rg
+       , IMutator* m, ICrossover* c, ISelector* s
+       , unordered_map<string, double> extra
+       , double f , double cr , size_t np , size_t max_iter)
+    : _func(func), _ranges(rg), _f(f), _cr(cr), _curr_gen(0), _np(np), _dim(rg.size()), _max_iter(max_iter)
+    , _extra_conf(extra), _mutator(m), _crossover(c), _selector(s), _use_built_in_strategy(true)
+{}
+Solution DE::solver()
+{
+    init();
+    report_best();
+    for (_curr_gen = 1; _curr_gen < _max_iter; ++_curr_gen)
+    {
+        auto doners = _mutator->mutation(*this);
+        auto trials = _crossover->crossover(*this, _population, doners);
+        vector<Evaluated> trial_results(_np);
+        transform(trials.begin(), trials.end(), trial_results.begin(), _func);
+        auto new_result = _selector->select(*this, _population, trials, _results, trial_results);
+        copy(new_result.first.begin(),  new_result.first.end(),  _results.begin());
+        copy(new_result.second.begin(), new_result.second.end(), _population.begin());
+        report_best();
+    }
+    size_t best_idx = find_best();
+    return _population[best_idx];
+}
+DE::~DE()
+{
+    if (_use_built_in_strategy)
+    {
+        assert(_mutator != nullptr && _crossover != nullptr && _selector != nullptr);
+        delete _mutator;
+        delete _crossover;
+        delete _selector;
+    }
+}
+void DE::set_mutator(MutationStrategy ms, const unordered_map<string, double>& config)
+{
+    switch (ms)
+    {
+    case Rand1:
+        _mutator = new Mutator_Rand_1;
+        break;
+    case Best1:
+        _mutator = new Mutator_Best_1;
+        break;
+    case Best2:
+        _mutator = new Mutator_Best_2;
+        break;
+    case RandToBest1:
+        _mutator = new Mutator_RandToBest_1;
+        break;
+    default:
+        _mutator = nullptr;
+        cerr << "Unrecognoized Mutation Strategy" << endl;
+        exit(EXIT_FAILURE);
+    }
+}
+void DE::set_crossover(CrossoverStrategy cs, const unordered_map<string, double>& config)
+{
+    if (cs == CrossoverStrategy::Bin)
+        _crossover = new Crossover_Bin;
+    else if (cs == CrossoverStrategy::Exp)
+        _crossover = new Crossover_Exp;
+    else
+    {
+        _crossover = nullptr;
+        cerr << "Unrecognoized Crossover Strategy" << endl;
+        exit(EXIT_FAILURE);
+    }
+}
+void DE::set_selector(SelectionStrategy ss, const unordered_map<string, double>& config)
+{
+    if (ss == SelectionStrategy::StaticPenalty)
+        _selector = new Selector_StaticPenalty;
+    else if (ss == SelectionStrategy::FeasibilityRule)
+        _selector = new Selector_FeasibilityRule;
+    else if (ss == SelectionStrategy::Epsilon)
+    {
+        for (string name : vector<string> {"theta", "tc", "cp"})
+            assert(config.find(name) != config.end());
+        double theta = config.find("theta")->second;
+        size_t tc = config.find("tc")->second;
+        size_t cp = config.find("cp")->second;
+        _selector = new Selector_Epsilon(theta, tc, cp);
+    }
+    else
+    {
+        _crossover = nullptr;
+        cerr << "Unrecognoized Selection Strategy" << endl;
+        exit(EXIT_FAILURE);
+    }
+}
+void DE::init()
+{
+    // rate of populations with non-infinity constraint violationss
+    _population = vector<Solution>(_np, vector<double>(_dim, 0));
+    _results    = vector<Evaluated>(_np);
+    double theta = _extra_conf.find("theta") == _extra_conf.end() ? _extra_conf.find("theta")->second : 0;
+    if (theta < 0) theta = 0;
+    if (theta > 1) theta = 1;
+    size_t min_valid_num = (size_t)(_np * theta);
+    vector<uniform_real_distribution<double>> range_distr(_dim);
+    for (size_t i =  0; i < _dim; ++i)
+        range_distr[i] = uniform_real_distribution<double>(_ranges[i].first, _ranges[i].second);
+    vector<bool> valid(_np, false);
+    size_t num_valid = 0;
+    while (num_valid < min_valid_num)
+    {
+        #pragma omp parallel for reduction(+:num_valid)
+        for (size_t i = 0; i < _np; ++i)
+        {
+            if (! valid[i])
+            {
+                #pragma omp critical
+                {
+                    for (size_t j = 0; j < _dim; ++j)
+                    {
+                        _population[i][j] = range_distr[j](engine);
+                    }
+                }
+                _results[i] = _func(_population[i]);
+                bool valid_flag = true;
+                for (auto vio : _results[i].second)
+                {
+                    if (std::isinf(vio)) valid_flag = false;
+                }
+                valid[i] = valid_flag;
+                num_valid += valid_flag ? 1 : 0;
+            }
+        }
+    }
+}
+size_t DE::find_best() const noexcept
+{
+    auto min_iter = min_element(_results.begin(), _results.end(), [&](const Evaluated & e1, const Evaluated & e2) -> bool
+    {
+        return _selector->better(e1, e2);
+    });
+    return distance(_results.begin(), min_iter);
+}
+void DE::report_best() const noexcept
+{
+    size_t best_idx = find_best();
+    const Evaluated& best_result = _results[best_idx];
+    const double violation = accumulate(best_result.second.begin(), best_result.second.end(), 0);
+    cout << "Best idx: " << best_idx << ", Best FOM: " << best_result.first << ", Constraint Violation: " << violation << endl;
 }
--- inc/DifferentialEvolution.h
+++ inc/DifferentialEvolution.h
@@ -18,42 +18,46 @@ enum SelectionStrategy {StaticPenalty = 0, FeasibilityRule, Epsilon};
 class IMutator
 {
 public:
-    virtual std::vector<Solution> mutation(const DE&, const std::vector<Solution>&) = 0;
+    virtual std::vector<Solution> mutation(const DE&) = 0;
+    virtual double boundary_constraint(std::pair<double, double>, double) const noexcept;
+    virtual ~IMutator(){}
 };
 class ICrossover
 {
 public:
     virtual std::vector<Solution> crossover(const DE&, const std::vector<Solution>&, const std::vector<Solution>&) = 0;
+    virtual ~ICrossover(){}
 };
 class ISelector
 {
 public:
     virtual bool better(const Evaluated&, const Evaluated&) = 0;
     virtual std::pair<std::vector<Evaluated>, std::vector<Solution>> select(const DE&      // There'll be a default implementation
-                                                                            , const std::vector<Solution>&
-                                                                            , const std::vector<Solution>&
-                                                                            , const std::vector<Evaluated>&
-                                                                            , const std::vector<Evaluated>&);
+            , const std::vector<Solution>&
+            , const std::vector<Solution>&
+            , const std::vector<Evaluated>&
+            , const std::vector<Evaluated>&);
+    virtual ~ISelector(){}
 };
 class Mutator_Rand_1 : public IMutator
 {
 public:
-    std::vector<Solution> mutation(const DE&, const std::vector<Solution>&);
+    std::vector<Solution> mutation(const DE&);
 };
 class Mutator_Best_1 : public IMutator
 {
 public:
-    std::vector<Solution> mutation(const DE&, const std::vector<Solution>&);
+    std::vector<Solution> mutation(const DE&);
 };
 class Mutator_Best_2 : public IMutator
 {
 public:
-    std::vector<Solution> mutation(const DE&, const std::vector<Solution>&);
+    std::vector<Solution> mutation(const DE&);
 };
 class Mutator_RandToBest_1 : public IMutator
 {
 public:
-    std::vector<Solution> mutation(const DE&, const std::vector<Solution>&);
+    std::vector<Solution> mutation(const DE&);
 };
 class Crossover_Bin : public ICrossover
 {
@@ -65,33 +69,39 @@ class Crossover_Exp : public ICrossover
 public:
     std::vector<Solution> crossover(const DE&, const std::vector<Solution>&, const std::vector<Solution>&);
 };
-class Selector_StaticPenalty : ISelector
+class Selector_StaticPenalty : public ISelector
 {
 public:
     bool better(const Evaluated&, const Evaluated&);
 };
-class Selector_FeasibilityRule : ISelector
+class Selector_FeasibilityRule : public ISelector
 {
 public: // perhaps it would be better if this class inherits Selector_Epsilon and set epsilon_0 = 0
     bool better(const Evaluated&, const Evaluated&);
 };
-class Selector_Epsilon : ISelector
+class Selector_Epsilon : public ISelector
 {
+    const double theta;
+    const size_t tc;
+    const size_t cp;
     double epsilon_0;
-    double theta;
-    size_t tc;
-    size_t cp;
     double epsilon_level;
 public:
     bool better(const Evaluated&, const Evaluated&);
     std::pair<std::vector<Evaluated>, std::vector<Solution>> select(const DE&      // There'll be a default implementation
-                                                                    , const std::vector<Solution>&
-                                                                    , const std::vector<Solution>&
-                                                                    , const std::vector<Evaluated>&
-                                                                    , const std::vector<Evaluated>&);
-    Selector_Epsilon(double ep0, double theta, size_t tc, size_t cp)
-        : epsilon_0(ep0), theta(theta), tc(tc), cp(cp), epsilon_level(ep0)
-    {}
+            , const std::vector<Solution>&
+            , const std::vector<Solution>&
+            , const std::vector<Evaluated>&
+            , const std::vector<Evaluated>&);
+    Selector_Epsilon(double theta, size_t tc, size_t cp)
+        : theta(theta), tc(tc), cp(cp)
+    {
+        if(theta < 0 || theta > 1)
+        {
+            std::cerr << "theta should be in [0, 1]" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
 };
 
 class DE
@@ -105,33 +115,50 @@ protected:
     const size_t _np;
     const size_t _dim;
     const size_t _max_iter;
+    const std::unordered_map<std::string, double> _extra_conf;
     std::vector<Solution>  _population; // use vector to represent a 2D matrix might not be efficient
     std::vector<Evaluated> _results;
 
     IMutator*   _mutator;
     ICrossover* _crossover;
     ISelector*  _selector;
+    bool _use_built_in_strategy;
+    void set_mutator(MutationStrategy,    const std::unordered_map<std::string, double>&);
+    void set_crossover(CrossoverStrategy, const std::unordered_map<std::string, double>&);
+    void set_selector(SelectionStrategy,  const std::unordered_map<std::string, double>&);
     void init();
-    void set_mutator();
-    void set_corssover();
-    void set_selector();
 public:
-    DE(Objective,
+    DE(Objective, // User-defined strategy, and strategy pointers would be destructed by user
        const Ranges&,
+       IMutator* m,
+       ICrossover* c,
+       ISelector* s,
+       std::unordered_map<std::string, double> extra_para = std::unordered_map<std::string, double> {},
        double f          = 0.8,
        double cr         = 0.8,
        size_t np         = 100,
-       size_t max_iter   = 200,
+       size_t max_iter   = 200);
+    DE(Objective,
+       const Ranges&,
        MutationStrategy  = Best1,
        CrossoverStrategy = Bin,
        SelectionStrategy = StaticPenalty,
-       std::unordered_map<std::string, double> extra_para = std::unordered_map<std::string, double> {});
+       std::unordered_map<std::string, double> extra_para = std::unordered_map<std::string, double> {},
+       double f          = 0.8,
+       double cr         = 0.8,
+       size_t np         = 100,
+       size_t max_iter   = 200);
     ~DE();
     Solution solver();
+
     double f()         const noexcept { return _f; }
     double cr()        const noexcept { return _cr; }
     size_t np()        const noexcept { return _np; }
     size_t curr_gen()  const noexcept { return _curr_gen; }
     size_t dimension() const noexcept { return _dim; }
-    size_t find_best(const std::vector<Solution>&) const noexcept;
+    std::pair<double, double> range(size_t i) const { return _ranges.at(i);}
+    const std::vector<Solution>& population() const noexcept { return _population; }
+    const std::vector<Evaluated>& evaluated() const noexcept { return _results; }
+    size_t find_best() const noexcept;
+    void report_best() const noexcept;
 };
