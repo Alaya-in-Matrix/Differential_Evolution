618b13f7b3f8d9510db6dc194dfa7c0a680a8d69
alaya <lvwenlong_lambda@qq.com>
2016-03-17 22:36:06 +0800

    Minor refactor on function `random_exclusive`

--- inc/util.h
+++ inc/util.h
@@ -4,30 +4,19 @@
 #include <vector>
 #include <random>
 #include <string>
+#include <algorithm>
+#include <cassert>
 template <typename Integral>
 Integral random_exclusive(std::uniform_int_distribution<Integral>& distr,
                           const std::vector<Integral>& exclude = std::vector<Integral>{})
 {
-    using namespace std;
-    if (exclude.size() >= (distr.b() - distr.a() + 1))
-    {
-        cerr << "random_exclusive, exclusive list greater than random range!" << endl;
-        exit(EXIT_FAILURE);
-    }
+    assert(exclude.size() < (distr.b() - distr.a() + 1));
     Integral ret;
     bool replicate = false;
     do
     {
-        ret = distr(engine);
-        replicate = false;
-        for (const auto v : exclude)
-        {
-            if (ret == v)
-            {
-                replicate = true;
-                break;
-            }
-        }
+        ret       = distr(engine);
+        replicate = std::find(exclude.begin(), exclude.end(), ret) != exclude.end();
     } while (replicate);
     return ret;
 }
